f4030e87d79fed1228bb5cf917c6623136996fc6
xmoonanx <xmoonanx@gmail.com>
2021-09-01 14:33:43 +0900

    21_09_01

--- 3052.cpp
+++ 3052.cpp
@@ -6,23 +6,18 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int in, result = 0;
-    int remainder[10];
+   int in, cnt = 0;
+   int remain[42] = {0};
 
-    for (int i = 0; i < 10; i++)
-    {
-        cin >> in;
-        remainder[i] = (in % 42);
-        
-    }
+   for(int i = 0; i< 10; i++) {
+       cin >> in;
+       remain[in % 42] = 1;
+   }
 
-    for(int i = 0; i < 10; i++) {
-        int cnt = 0;
-        for(int j = i+1; j < 10; j++){
-            if(remainder[i] == remainder[j]) cnt++;
-        }
-        if (cnt == 0) result++;
-    }
+   for(int i = 0; i<42; i++){
+       if(remain[i] == 1) cnt++;
+   }
 
-    cout << result;
+   cout << cnt;
+
 }
