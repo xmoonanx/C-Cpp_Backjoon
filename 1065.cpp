ba83e2f6887625403cdac7619c9403974479a538
xmoonanx <xmoonanx@gmail.com>
2021-09-04 13:56:19 +0900

    21_09_04

--- 1065.cpp
+++ 1065.cpp
@@ -1,11 +1,35 @@
 #include <iostream>
 using namespace std;
 
+bool han(int n){
+    if(n < 100){
+        return true;
+    }
+    int n1, n2, n3;
+    n1 = n / 100;
+    n2 = n % 100 / 10;
+    n3 = n % 10;
 
-
-int main(){
-
+    if(n1-n2==n2-n3){
+        return true;
+    }
+    return false;
 }
 
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-// 1 ~ 110까지 한수 아닌거 : 100, 101, 102 103, 104, 105, 106, 107, 108, 109, 110 
+    int n, cnt=0;
+    cin >> n;
+    for(int i = 1; i <= n; i++){
+        if(han(i)==true){
+            cnt++;
+        }
+    }
+    cout << cnt;
+}
+
+// 1 ~ 110까지 한수 : 1~99 : 99개
+// 1 ~ 210까지 한수 : 1~99, 111, 123, 135, 147, 159, 210 : 105개
+//
