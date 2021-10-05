5dade0f45ca3724934cea3dcce5232f43315c5d1
xmoonanx <xmoonanx@gmail.com>
2021-10-05 14:16:25 +0900

    21_10_05

---
+++ 2738.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int arr[101][101];
+int arr1[101][101];
+
+int main()
+{
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> arr1[i][j];
+            arr[i][j] += arr1[i][j];
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << "\n";
+    }
+}
