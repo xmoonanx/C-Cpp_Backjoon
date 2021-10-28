b0630199f11cb7d28cb6cd70a365f6d9c7ee3f4d
xmoonanx <xmoonanx@gmail.com>
2021-10-28 12:59:06 +0900

    21_10_28

---
+++ 10870.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int fibo(int n)
+{
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+    return fibo(n - 2) + fibo(n - 1);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << fibo(n);
+}
